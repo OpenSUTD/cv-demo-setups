b6b34a931dcb57c2822cf3c84ba480003f660a61
Timothy Liu <tlkh.xms@gmail.com>
2019-07-19 10:46:20 +0800

    Update yogapose project

--- yogapose/demo.py
+++ yogapose/demo.py
@@ -23,7 +23,7 @@ except ImportError as e:
 
 # Flags
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_folder', type=str, default='../openpose/models/')
+parser.add_argument('--model_folder', type=str, default='./openpose/models/')
 parser.add_argument('--target_video', type=str, default='./yoga.mp4')
 parser.add_argument('--net_resolution', type=str, default='176x176')
 parser.add_argument('--cam_width', type=int, default=1920)
@@ -107,9 +107,13 @@ def get_ordinal_score(score):
 
 
 def cropped_image(full_image):
-    w_min = 960 - (args.display_width // 4)
-    w_max = 960 + (args.display_width // 4)
-    out = full_image[0:args.display_height, w_min:w_max]
+    try:
+        w_min = 960 - (args.display_width // 4)
+        w_max = 960 + (args.display_width // 4)
+        out = full_image[0:args.display_height, w_min:w_max]
+    except Exception as e:
+        print(e)
+        out = full_image
     return out
 
 
@@ -145,87 +149,94 @@ while True:
     # Get image from webcam
     frames += 1
     ret, img_original = stream.read()
-    img = cv2.flip(cropped_image(img_original), 1)
-
-    # Get image from target video
-    target_ret, stream_target_img_original = stream_target.read()
-    stream_target_img = cv2.flip(cropped_image(stream_target_img_original), 1)
-    if img is None:
+    if img_original is None:
         continue
 
-    # Label image
-    datum = op.Datum()
-    datum.cvInputData = img
-    opWrapper.waitAndEmplace([datum])
-    opWrapper.waitAndPop([datum])
+    try:
+        img = cv2.flip(cropped_image(img_original), 1)
 
-    # Label target image
-    datum_target = op.Datum()
-    datum_target.cvInputData = stream_target_img
-    opWrapper.waitAndEmplace([datum_target])
-    opWrapper.waitAndPop([datum_target])
+        # Get image from target video
+        target_ret, stream_target_img_original = stream_target.read()
+        if stream_target_img_original is None:
+            continue
+        stream_target_img = cv2.flip(cropped_image(stream_target_img_original), 1)
 
-    # Check if OpenPose managed to label
-    if type(datum.poseKeypoints) != np.ndarray or \
-       datum.poseKeypoints.shape != (1, 25, 3):
-        continue
+        # Label image
+        datum = op.Datum()
+        datum.cvInputData = img
+        opWrapper.waitAndEmplace([datum])
+        opWrapper.waitAndPop([datum])
 
-    elif type(datum_target.poseKeypoints) != np.ndarray or \
-         datum_target.poseKeypoints.shape != (1, 25, 3):
-        continue
+        # Label target image
+        datum_target = op.Datum()
+        datum_target.cvInputData = stream_target_img
+        opWrapper.waitAndEmplace([datum_target])
+        opWrapper.waitAndPop([datum_target])
 
-    # Scale, transform, normalize, reshape, predict
-    coords_vec = make_vector(datum.poseKeypoints)
-    target_coords_vec = make_vector(datum_target.poseKeypoints)
-    input_vec = np.concatenate([coords_vec, target_coords_vec]).flatten()
-    similarity_score = model.predict(input_vec.reshape((1, -1)))
-    ordinal_score = get_ordinal_score(similarity_score)
+        # Check if OpenPose managed to label
+        if type(datum.poseKeypoints) != np.ndarray or \
+        datum.poseKeypoints.shape != (1, 25, 3):
+            continue
 
-    # Fit target video into stream target window
-    stream_target_window[0:0 + datum_target.cvOutputData.shape[0],
-    0:0 + datum_target.cvOutputData.shape[1]] = datum_target.cvOutputData
+        elif type(datum_target.poseKeypoints) != np.ndarray or \
+            datum_target.poseKeypoints.shape != (1, 25, 3):
+            continue
 
-    # Concatenate webcam and target video
-    numpy_horizontal_concat = np.concatenate((datum.cvOutputData,
-                                              stream_target_window),
-                                              axis=1)
+        # Scale, transform, normalize, reshape, predict
+        coords_vec = make_vector(datum.poseKeypoints)
+        target_coords_vec = make_vector(datum_target.poseKeypoints)
+        input_vec = np.concatenate([coords_vec, target_coords_vec]).flatten()
+        similarity_score = model.predict(input_vec.reshape((1, -1)))
+        ordinal_score = get_ordinal_score(similarity_score)
 
-    # Add overlay to show results
-    overlay = numpy_horizontal_concat.copy()
-    cv2.rectangle(overlay, (0, 0), (args.display_width // 2, args.display_height),
-                  get_ordinal_score(similarity_score)[2], -1)
-    numpy_horizontal_concat = cv2.addWeighted(overlay, ordinal_score[1],
-                                              numpy_horizontal_concat,
-                                              1 - ordinal_score[1], 0,
-                                              numpy_horizontal_concat)
+        # Fit target video into stream target window
+        stream_target_window[0:0 + datum_target.cvOutputData.shape[0],
+        0:0 + datum_target.cvOutputData.shape[1]] = datum_target.cvOutputData
 
-    # Draw a vertical white line with thickness of 10 px
-    cv2.line(numpy_horizontal_concat, (args.display_width // 2, 0),
-             (args.display_width // 2, args.display_height),
-             (255, 255, 255), 10)
+        # Concatenate webcam and target video
+        numpy_horizontal_concat = np.concatenate((datum.cvOutputData,
+                                                stream_target_window),
+                                                axis=1)
 
-    # Display comment
-    cv2.rectangle(numpy_horizontal_concat, (10, 30), (600, 120), (255, 255, 255), 3)
-    font = cv2.FONT_HERSHEY_DUPLEX
-    cv2.putText(numpy_horizontal_concat, ' ' + ordinal_score[0], (10, 100), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
+        # Add overlay to show results
+        overlay = numpy_horizontal_concat.copy()
+        cv2.rectangle(overlay, (0, 0), (args.display_width // 2, args.display_height),
+                    get_ordinal_score(similarity_score)[2], -1)
+        numpy_horizontal_concat = cv2.addWeighted(overlay, ordinal_score[1],
+                                                numpy_horizontal_concat,
+                                                1 - ordinal_score[1], 0,
+                                                numpy_horizontal_concat)
 
-    # Record Video
-    out.write(numpy_horizontal_concat)
+        # Draw a vertical white line with thickness of 10 px
+        cv2.line(numpy_horizontal_concat, (args.display_width // 2, 0),
+                (args.display_width // 2, args.display_height),
+                (255, 255, 255), 10)
 
-    # Display img
-    cv2.imshow("Webcam and Target Image", numpy_horizontal_concat)
+        # Display comment
+        cv2.rectangle(numpy_horizontal_concat, (10, 30), (600, 120), (255, 255, 255), 3)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(numpy_horizontal_concat, ' ' + ordinal_score[0], (10, 100), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
 
-    # Check for quit
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+        # Record Video
+        out.write(numpy_horizontal_concat)
 
-    # Print frame rate
-    if time.time() - start >= 1:
-        framerate = frames
-        print('Framerate: ', framerate)
-        frames = 0
-        start = time.time()
+        # Display img
+        cv2.imshow("Webcam and Target Image", numpy_horizontal_concat)
+
+        # Check for quit
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+        # Print frame rate
+        if time.time() - start >= 1:
+            framerate = frames
+            print('Framerate: ', framerate)
+            frames = 0
+            start = time.time()
+
+    except Exception as e:
+        print(e)
 
 # Clean up
 stream.release()
